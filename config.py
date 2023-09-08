e0076b222f003dbe7441581c4532c2a56ce115d2
Kktbots <143538428+Kktbots@users.noreply.github.com>
2023-09-08 09:32:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6232350725:AAFFT1PF5eKJYgTtuHt_DPFrM_u9kJflals")
-    API_ID = int(os.environ.get("API_ID", "10577960"))
-    API_HASH = os.environ.get("API_HASH", "80fd047285f4e94ca80311928b6bb5da")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5838655940:AAEh36OBWskzA97ZOdBr98390qIHFQ9sGe8")
+    API_ID = int(os.environ.get("API_ID", "29540156"))
+    API_HASH = os.environ.get("API_HASH", "775d47859519f34f01549344a392280b")
     
 
